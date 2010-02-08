e7e9ca2b417cc59b1484066baf5bbc904879eb3c
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-02-08 15:47:16 -0500

    asm macro basically works now.

--- compiler/macro.lisp
+++ compiler/macro.lisp
@@ -1,27 +1,29 @@
-(defconstant +addressing-modes-and-syntax+
-    '((:implied "")
-      (:accumulator "A")
-      (:immediate "#")
-      (:direct "b")
-      (:direct-x-indexed "b,X")
-      (:direct-y-indexed "b,Y")
-      (:stack-indexed "b,S")
-      (:direct-indirect "(b)")
-      (:direct-indirect-long "[b]")
-      (:direct-x-indexed-indirect "(b,X)")
-      (:direct-indirect-y-indexed "(b),Y")
-      (:direct-indirect-long-y-indexed "[b],Y")
-      (:stack-relative-inderect-y-indexed "(b,S),Y")
-      (:block-mode "b,b")
-      (:absolute "w")
-      (:absolute-x-indexed "w,X")
-      (:absolute-y-indexed "w,Y")
-      (:absolute-indirect "(w)")
-      (:absolute-indirect-long "[w]")
-      (:absolute-x-indexed-indirect "(w,X)")
-      (:long "l")
-      (:long-x-indexed "l,X")))
+(defparameter +addressing-modes-and-syntax+
+  '((:implied "")
+    (:accumulator "A")
+    (:immediate "#")
+    (:direct "b")
+    (:direct-x-indexed "b,X")
+    (:direct-y-indexed "b,Y")
+    (:stack-indexed "b,S")
+    (:direct-indirect "(b)")
+    (:direct-indirect-long "[b]")
+    (:direct-x-indexed-indirect "(b,X)")
+    (:direct-indirect-y-indexed "(b),Y")
+    (:direct-indirect-long-y-indexed "[b],Y")
+    (:stack-relative-indirect-y-indexed "(b,S),Y")
+    (:block-move "b,b")
+    (:absolute "w")
+    (:absolute-x-indexed "w,X")
+    (:absolute-y-indexed "w,Y")
+    (:absolute-indirect "(w)")
+    (:absolute-indirect-long "[w]")
+    (:absolute-x-indexed-indirect "(w,X)")
+    (:long "l")
+    (:long-x-indexed "l,X")))
 
+(defun flatten (lists) (apply #'append lists))
+(defun lookup (dict key) (second (assoc key dict)))
 
 "
 Everything after this is untested.  It is just to get a good idea of
@@ -29,22 +31,35 @@ how best to layout this code.
 "
 
 (defun asm-subformat (format-char argument)
-  (format nil (ecase format-char (#\b "$~2x") (#\w "$~s") (#\l "$~4x"))
-              argument))
+  (etypecase argument
+    (symbol (symbol-name argument))
+    (number (format nil (ecase format-char (#\b "~db") (#\w "~dw") (#\l "~dl"))
+                    argument))))
 
 (defun asm-format (format-string &rest arguments)
   "Take an asm form from +addressing-modes-and-syntax+ and some arguements,
    returning the corresponding asm syntax.  "
-  (coerse 'string
-    (flatten 
-      (loop for char in format-string
-            collect (if (find char '(#\b #\w #\l))
-                      (coerce (asm-subformat char (pop arguments)) 'list)
-                      (list char))))))
+  (coerce (flatten
+           (loop for char across format-string
+              collect (if (find char '(#\b #\w #\l))
+                          (coerce (asm-subformat char (pop arguments)) 'list)
+                          (list char))))
+          'string))
+
+(defun emit (string) (format t "~a" string) (values))
 
 (defmacro asm (command mode &rest args)
-  (list emit (format nil "; ~s ~s"
-               (symbol-name command)
-               (apply #'asm-format
-                      (first (assoc mode +addressing-modes-and-syntax+))
-                args))))
+  (list 'emit (format nil "; ~a ~a~%"
+                      (symbol-name command)
+                      (apply #'asm-format
+                             (lookup +addressing-modes-and-syntax+ mode)
+                             args))))
+
+
+"
+  (asm lda :absolute 3)
+  (asm clc :implied)
+  (asm adc :direct 4)
+"
+
+(loop do (eval (read)))
