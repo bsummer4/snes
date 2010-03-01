c1a62c06ff66f8ac2a68fb021fd71c35c0de9b20
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-03-01 12:49:11 -0500

    Whatever

--- compiler/compiler.lisp
+++ compiler/compiler.lisp
@@ -1,19 +1,42 @@
 #!/usr/bin/sbcl --script
 
-"A unix interface to the compiler.  "
+"
+A unix interface to the c compiler.
 
+If you want to dump a core image, define *script?* to nil before
+loading this file.  Otherwise we simply run the c-compiler as script.
+"
+
+(defvar *script?* t)
 (require :cs400-compiler)
 (in-package :cs400-compiler)
 
-(defun main (&optional program-name &rest args)
-  (declare (ignore args program-name))
-  (handler-case
-      (let ((eof '#.(gensym "EOF-")))
-        (loop for x = (read *standard-input* nil eof)
-           until (eq x eof) do (eval x)))
-    (error (e)
-      (format *error-output* "Fatal Error:~%    ~a~%" e)
-      1))
+(defun repl ()
+  (let ((eof '#.(gensym "EOF-")))
+    (loop for x = (read *standard-input* nil eof)
+       until (eq x eof) do (eval x)
+       do (force-output))))
+
+(defun main (args)
+  (declare (ignore args))
+  (repl)
   0)
 
-(sb-ext:quit :unix-status (apply #'main sb-ext:*posix-argv*))
+(defun top ()
+  (in-package :cs400-compiler)
+  (sb-ext:quit
+   :unix-status
+   (handler-case (apply #'main sb-ext:*posix-argv*)
+     (error (e)
+       (format *error-output* "Fatal Error:~% ~a~%" e)
+       2)
+     (t (s)
+       (declare (ignore s))
+       1))))
+
+(if cl-user::*script?*
+    (top)
+    (sb-ext:save-lisp-and-die "compiler"
+                              :executable t
+                              :toplevel #'top
+                              :purify t))
